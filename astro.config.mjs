09deefd853457245f3efe418c8eb0941782a12a6
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-01-31 23:21:42 +0100

    Reset the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [vanillaExtractPlugin({})]
-  },
-  output: "static",
-  adapter: vercel({
-    analytics: true,
-  })
-});
+export default defineConfig({});
--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
